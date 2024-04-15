28bdab6b45d079667566008ffe8a164ed84f7f50
Mitzi Estrada <mitziestrada@Mitzis-Air.lan>
2024-04-14 22:08:51 -0700

    import Routes and Route, create routes for SigninForm and Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 // This is the primary routing component
-
+// By wrapping BrowserRouter around App, we can import Routes and Route in App
+import { Routes, Route } from "react-router-dom";
 import "./globals.css";
 
 export const App = () => {
-  return <div className="text-3xl font-bold underline">Hello FaceGram!</div>;
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* public routes*/}
+        <Route path="/sign-in" element={<SigninForm />} />
+
+        {/* private routes*/}
+        <Route index element={<Home />} />
+      </Routes>
+    </main>
+  );
 };
