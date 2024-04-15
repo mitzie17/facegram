09eb073c92a93388fddadffe5f5fdff17d7a32ef
Mitzi Estrada <mitziestrada@Mitzis-Air.lan>
2024-04-14 21:05:56 -0700

    create react app using vite

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+// This is the primary routing component
+
+export const App = () => {
+  return <div>App</div>;
+};
---
+++ src/main.tsx
@@ -0,0 +1,6 @@
+// This is the starting point of the application
+
+import ReactDOM from "react-dom/client";
+import { App } from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
